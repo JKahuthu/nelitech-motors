d796c023762c5e4b94ef602e262961551ee4ddb2
John Kahuthu <kahuthujmk@gmail.com>
2025-07-24 07:13:11 +0300

    Moved to src folder and added main.jsx

---
+++ app/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NelitechMotors from './App.jsx';
+import './index.css'; // optional, only if you want styles
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NelitechMotors />
+  </React.StrictMode>
+);
+
